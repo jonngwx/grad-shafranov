ad3108e2a36e812614ae90e4b8509a96e0f02e5c
Jonathan Ng <jonngwx@gmail.com>
2014-12-02 15:53:01 -0500

    comments in field, grad_output_txt

--- include/field.h
+++ include/field.h
@@ -3,21 +3,31 @@
 
 #include <stdlib.h>
 
+/**
+ * \file Header file for Field class
+ * */
+
 class Field(){
 	public:
 	Field(double R0, double Rend, double z0, double zend, int nr, int nz);
 	~Field();
 	
-	const int nr;
-	const int nz;
-	double *R;
-	double *z;
-	double **f;
-	double **f_old;
-	double dr;
-	double dz;
+	const int nr; /** < number of points in R direction */
+	const int nz; /** < number of points in z direction */
+	double *R; /** < pointer to array of radial grid points */
+	double *z; /** < pointer to array of vertical grid points */
+	double **f; /** < pointer to field data */
+	double **f_old; /** < pointer to old field data */
+	double dr; /** < grid spacing in radial direction */
+	double dz; /** < grid spacing in vertical direction */
 	
-	private:
+private:
+	/**
+	 * Returns a the value of the ith boundary cell
+	 * TODO: do we really need this?
+	 * 
+	 * @param i index on the boundary?
+	 * */
 	double f_boundary(int i);
 };
 
--- include/grad_output_txt.h
+++ include/grad_output_txt.h
@@ -1,15 +1,31 @@
 #ifndef GRAD_OUTPUT_TXT
 #define GRAD_OUTPUT_TXT
 
+
+/**
+ * \file Header file for grad_output_txt
+ * */
+ 
 #include <stdlib.h>
 #include "grad_output.h"
 #include "field.h"
 
 class Grad_Output_Txt : public Grad_Output{
-	public:
+public:
+	/**
+	 * Constructor for output class
+	 * @param f pointer to field containing flux function
+	 * @param p pointer to field containing pressure function
+	 * @param g pointer to field containing g function
+	 * @param outputs string of comma separated output options
+	 * */
 	Grad_Output_Txt(Field* f, Field* p, Field* g, const char* outputs);
 	~Grad_Output_Txt();
 	
+	/**
+	 * Writes output to file.
+	 * @param filename name of output file
+	 * */
 	write_output(const char* filename);
 	
 };
