196a8e7b5f593e4780bfc377b625e65b65200771
Jacob Schwartz <jacobas@princeton.edu>
2014-12-01 19:58:26 -0500

    csv_reader now puts the variables into a struct that contains arrays

--- csvReader/csv_reader.cc
+++ csvReader/csv_reader.cc
@@ -7,8 +7,18 @@
 #include <iterator>
 #include <string>
 #include <vector>
+#include <assert.h>
 #include <iostream>
 #include <fstream>
+
+struct TsvData {
+  double * r_locations;
+  double * z_locations;
+  double * currents;
+  int num_entries;
+};
+
+typedef struct TsvData TsvData;
 
 int main(int argc, char* argv[]) {
   if (argc != 2) {
@@ -21,31 +31,52 @@ int main(int argc, char* argv[]) {
 
   std::ifstream filein(input_file_name.c_str());
   std::string line;
-  
+
   if (!filein.is_open()) {
     perror("Error while opening file");
   }
+  std::vector< std::vector<double> > tsv_table;
   while (std::getline(filein, line)) {
     std::istringstream ss(line);
     std::istream_iterator<std::string> begin(ss), end;
-    
+
     //putting all the tokens in the vector  
     std::vector<std::string> arrayTokens(begin, end);
-    
+
     std::vector<double> doubleVector(arrayTokens.size());
     std::transform(arrayTokens.begin(), arrayTokens.end(), doubleVector.begin(), [](std::string const& val){ return std::stod(val);});
-    
+
     //Should we now delete the arrayTokens vector?
-    
-    //print 'em all out.
-    for ( int i = 0; i < doubleVector.size(); i++) {
-      std::cout << doubleVector[i] << " ";
-    }
-    std::cout << "\n";
-    
+
+    tsv_table.push_back(doubleVector);
+
   }
   if (filein.bad()) {
     perror("Error while reading file");
   }
+  
+  // convert vector of vectors into struct of 3 arrays.
+  TsvData * td = new TsvData();
+  assert (td != NULL);
+  
+  int num_tsv_lines = tsv_table.size();
+  td->r_locations  = new double[num_tsv_lines];
+  assert (td->r_locations != NULL);
+  td->z_locations  = new double[num_tsv_lines];
+  assert (td->z_locations != NULL);
+  td->currents = new double[num_tsv_lines];
+  td->num_entries = num_tsv_lines;
+  
+  	//To access values
+  for (std::vector<double>::size_type i = 0; i != tsv_table.size(); ++i) {
+    td->r_locations[i] = tsv_table[i][0];
+    td->z_locations[i] = tsv_table[i][1];
+    td->currents[i] = tsv_table[i][2];
+  }
+  
+  for (int i = 0; i < td->num_entries; ++i) {
+    printf("R is %f and Z is %f and I is %f\n", td->r_locations[i], td->z_locations[i], td->currents[i]);
+  }
+  
   return 0;
 }
