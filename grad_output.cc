b65a8d689733a6fa36f15ffeffe70979c6c9050e
Jonathan Ng <jonngwx@gmail.com>
2014-12-04 17:37:01 -0500

    added output parsing

--- grad_output.cc
+++ grad_output.cc
@@ -1,5 +1,27 @@
 #include "grad_output.h"
+#include <string.h>
+#include <stdio.h>
 
-void Grad_Output::parse_outputs(const char *outputs){
+/**
+ * \file Grad output base class implementation 
+ * */
 
+
+
+/**
+ * @brief Parses the string of outputs to write to file and returns a list.
+ * @param out comma separated string of outputs. currently allows p,bt,q
+ * 
+ */
+void Grad_Output::parse_outputs(const char *out){
+    int len = strlen(out);
+    char *outputs;
+    strcpy(outputs,out);
+    printf("%s",outputs);
+    char *tok = NULL;
+    tok = strtok(outputs, ",");
+	while (tok){
+        printf("output is %s\n", tok);
+        tok = strtok(NULL,",");
+    }
 }
