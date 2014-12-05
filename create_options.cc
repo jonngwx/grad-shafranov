7903251ec77bd89195f027dc896f9550a215d728
Jacob Schwartz <jacobas@princeton.edu>
2014-12-05 17:21:26 -0500

    Added the files create_option.cc & .h
    In order to use these, we'll need to write a bit of code in main().
    Now I need to actually define the correct options.
    
    and then of course add them to the Makefile and such.
    and add -lboost_program_options to the makefile.

---
+++ create_options.cc
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <fstream>
+#include "create_options.h"
+
+int CreateOptions(int ac, char * av[], po::options_description &visible, po::variables_map &vm) {
+
+  // Declare a group of options that will be 
+  // allowed only on command line
+  po::options_description generic("Generic options");
+  generic.add_options()
+      ("version,v", "print version std::string")
+      ("help", "produce help message")
+      ("config,c", po::value<std::string>()->default_value("grad-shafranov.cfg"),
+            "name of a file of a configuration.")
+      ;
+
+  // Declare a group of options that will be 
+  // allowed both on command line and in
+  // config file
+  po::options_description config("Configuration");
+  config.add_options()
+      ("optimization", po::value<int>()->default_value(10), 
+            "optimization level")
+      ("include-path,I", 
+           po::value< std::vector<std::string> >()->composing(), 
+           "include path")
+      ;
+
+  // Hidden options, will be allowed both on command line and
+  // in config file, but will not be shown to the user.
+  po::options_description hidden("Hidden options");
+  hidden.add_options()
+      ("input-file", po::value< std::vector<std::string> >(), "input file")
+      ;
+
+  po::options_description cmdline_options;
+  cmdline_options.add(generic).add(config).add(hidden);
+
+  po::options_description config_file_options;
+  config_file_options.add(config).add(hidden);
+
+  // Add to the visible options list (which will be returned
+  // for possible printing later)
+  visible.add(generic).add(config);
+  
+  po::positional_options_description p;
+  p.add("input-file", -1);
+  
+  //store in vm
+  store(po::command_line_parser(ac, av).
+        options(cmdline_options).positional(p).run(), vm);
+  notify(vm);
+  
+  std::string config_file = vm["config"].as<std::string>();
+  
+  std::ifstream ifs(config_file.c_str());
+  if (!ifs)
+  {
+    // we require a config file. its abscence will be dealt with in main
+  }
+  else
+  {
+      store(parse_config_file(ifs, config_file_options), vm);
+      notify(vm);
+  }
+
+  return 0;
+}
