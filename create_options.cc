f1a6f626399ac3488ac26a28a1d4a90f635e3d57
Jacob Schwartz <jacobas@princeton.edu>
2014-12-05 17:45:41 -0500

    added some options for us to create_options.cc

--- create_options.cc
+++ create_options.cc
@@ -19,18 +19,32 @@ int CreateOptions(int ac, char * av[], po::options_description &visible, po::var
   // config file
   po::options_description config("Configuration");
   config.add_options()
-      ("optimization", po::value<int>()->default_value(10), 
-            "optimization level")
-      ("include-path,I", 
-           po::value< std::vector<std::string> >()->composing(), 
-           "include path")
+      ("coil-data-name,c", 
+           po::value< std::string>()->default_value("coil_data.tsv"), 
+           "coil data file name")
+      ("magnetic-data-name,m",
+           po::value< std::string>()->default_value("magnetic_data.tsv"),
+           "magnetic data file name")
+      ("pgtype", po::value<std::string>, "p and p specification type")
+      ("g-filename", po::value<std::string>(), "g file name")
+      ("p-filename", po::value<std::string>(), "p file name")
+      ("max-iter-n,N", po::value<int>()->default_value(10), 
+            "maximum inner iterations")
+      ("error-tol-N,eN", po::value<double>()->default_value(1e-6),
+            "error tolerance inner loop")
+      ("max-iter-m,M", po::value<int>()->default_value(10),
+            "maximum outer iterations")
+      ("error-tol-M,eM", po::value<double>()->default_value(1e-6),
+            "error tolerance outer loop")
+      ("output-type", po::value<std::string>()->default_value("tsv"), "tsv or hdf5")
+      ("output name", po::value<std::string>()->default_value("cougar.out"), "prefix for output filename")
       ;
 
   // Hidden options, will be allowed both on command line and
   // in config file, but will not be shown to the user.
   po::options_description hidden("Hidden options");
   hidden.add_options()
-      ("input-file", po::value< std::vector<std::string> >(), "input file")
+      ("unused", po::value< std::vector<std::string> >(), "placeholder")
       ;
 
   po::options_description cmdline_options;
@@ -44,7 +58,7 @@ int CreateOptions(int ac, char * av[], po::options_description &visible, po::var
   visible.add(generic).add(config);
   
   po::positional_options_description p;
-  p.add("input-file", -1);
+  p.add("unused", -1);
   
   //store in vm
   store(po::command_line_parser(ac, av).
