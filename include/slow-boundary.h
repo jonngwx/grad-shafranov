ae3deaead62206f5f1b8066ab7b85409f05c7978
pbolgert <peter.bolgert@gmail.com>
2014-12-05 15:12:59 -0500

    update slow_boundary.h

--- include/slow-boundary.h
+++ include/slow-boundary.h
@@ -1,14 +1,17 @@
 #ifndef SLOWBOUNDARY_H_
 #define SLOWBOUNDARY_H_
 
+#include "field.h"
+#include "grid.h"
+#include "tsv_reader.h"
 #include "boundary.h"
 /* need to include header that defines TsvData */
 
 class SlowBoundary : public Boundary {
   public:
-    SlowBoundary(int nr, int nz, double dr, double dz, TsvData &cond_data); 
+    SlowBoundary(Grid &grid, TsvData &cond_data); 
     ~SlowBoundary();
-    int CalcB(Field &Psi);
+    int CalcB(Field &psi, Field &jphi);
   private:
     /* to be determined */
 
