6035b8c33853bf3cf8b387cd740253472481a4fc
Jonathan Ng <jonngwx@gmail.com>
2014-12-05 14:20:56 -0500

    plotting works

--- python/viz.py
+++ python/viz.py
@@ -1,2 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
+#import field
+import read_data
+
+def plot(filename,format):
+    if format == "txt":
+        F = read_data.read_text(filename)
+    else:
+        1+1
+    plt.figure(1)
+    plt.subplot(2,1,1)
+    plt.pcolormesh(F.R,F.z,np.transpose(F.psi))
+    plt.title('$\Psi$')
+    plt.xticks([])
+    plt.ylabel('z')
+    plt.colorbar()
+    plt.subplot(2,1,2)
+    plt.pcolormesh(F.R,F.z,np.transpose(F.p))
+    plt.title('p')
+    plt.xlabel('$R$')
+    plt.ylabel('z')
+    plt.colorbar()
+    plt.show()
+    return F
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print 'wrong number of arguments'
+    else:
+        plot(sys.argv[1],sys.argv[2])
