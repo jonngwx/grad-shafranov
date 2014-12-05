49a277d3ad07b20142aa49d771afe090fd83a6bc
pbolgert <peter.bolgert@gmail.com>
2014-12-05 09:53:47 -0500

    in gs_solver.cc, changed all Psi to lowercase

--- gs_solver.cc
+++ gs_solver.cc
@@ -13,9 +13,9 @@ int main(int argc, char *argv[]){
     int nr,nz;
     double R0, Rend, z0, zend;
     Grid *grid = new Grid(R0, Rend, z0, zend, nr, nz);
-    Field *Psi = new Field(nr,nz);
-    Field *Psi_prev = new Field(nr,nz);
-    Field *Psi_next = new Field(nr,nz);
+    Field *psi = new Field(nr,nz);
+    Field *psi_prev = new Field(nr,nz);
+    Field *psi_next = new Field(nr,nz);
     
     Field *jphi = new Field(nr,nz);
     RHSfunc *p = new RHSfunc(pgtype, pdata);
@@ -23,19 +23,19 @@ int main(int argc, char *argv[]){
 
     // Elliptic solver for inner loop
     EllipticSolver *solver = new SOR(grid, omega_init, epsilon);
-    Boundary *Psib = new SlowBoundary(nr,nz,Psi->dr, Psi->dz, coils);    
+    Boundary *psib = new SlowBoundary(nr,nz, psi->dr, psi->dz, coils);    
 
     /** determine which output type */
-    Grad_Output grad_output = new Grad_Output_Txt(Psi,grid,p,g,"this,is,a,test");
+    Grad_Output grad_output = new Grad_Output_Txt(psi,grid,p,g,"this,is,a,test");
     // solve stuff
     for (int m = 0; m < maxIterM, ++m){
-        Psib->CalcB(Psi); // PETER this should come after as the initial guess already has a self consistent boundary?
+        psib->CalcB(psi); // PETER this should come after as the initial guess already has a self consistent boundary?
         // test convergence
         
-        solver->init(*Psi);
+        solver->init(*psi);
         for (int n = 0; n < maxIterN, ++n) {
-            calc_jphi(*grid, *jphi, *Psi, *p, *g);
-            Psi = solver->step();
+            calc_jphi(*grid, *jphi, *psi, *p, *g);
+            psi = solver->step();
             if (solver->norm() < solver->epsilon()) break;
         }
     }
@@ -46,12 +46,12 @@ int main(int argc, char *argv[]){
 
     delete grad_output;
     delete grid;
-    delete Psi;
-    delete Psib;
+    delete psi;
+    delete psib;
     delete solver;
 }
 
-int calc_jphi(Grid *grid, Field *jphi, Field *Psi, RHSfunc *p, RHSfunc *g){
+int calc_jphi(Grid *grid, Field *jphi, Field *psi, RHSfunc *p, RHSfunc *g){
 
   //eventually this'll update the jphi field
 
