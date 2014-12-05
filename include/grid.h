bde18488b851c4bd70ff55a6c57a4e906f8d312f
Jonathan Ng <jonngwx@gmail.com>
2014-12-05 13:55:52 -0500

    fixing the renaming

--- include/grid.h
+++ include/grid.h
@@ -7,7 +7,7 @@ public:
     Grid(double R0, double Rend, double z0, double zend, int nr, int nz);
     ~Grid();
     double celli(double r);
-    double cellj(double z);
+    double cellj(double z_);
 	
     const int nr_; /** < number of points in R direction */
     const int nz_; /** < number of points in z direction */
