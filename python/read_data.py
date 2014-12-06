d5c29ac174ed659ad64392ea696418a6fd684bb9
Jonathan Ng <jonngwx@hopper12.(none)>
2014-12-05 20:29:40 -0800

    now plotting hdf

--- python/read_data.py
+++ python/read_data.py
@@ -1,10 +1,19 @@
 import numpy as np
 import io
 import field
-#import h5py
+import h5py
 
 def read_hdf5(filename):
-    return 0
+    f = h5py.File(filename,'r')
+    x = f['R'].shape[0]
+    y = f['z'].shape[0]
+    fields = field.Field(x,y)
+    fields.R = np.array(f['R'])
+    fields.z = np.array(f['z'])
+    fields.psi = np.transpose(np.array(f['psi']))
+    fields.p = np.transpose(np.array(f['p']))
+    f.close()
+    return fields
 
 
 def read_text(filename):
