941f0ecf632db806edacc6ecc54f063597bfc316
Jacob Schwartz <jacobas@princeton.edu>
2014-12-07 17:43:14 -0500

    Added some doxygen to create_options.h

--- include/create_options.h
+++ include/create_options.h
@@ -1,3 +1,8 @@
+/**
+ * @file create_options.h
+ * @author Jacob Schwartz
+ * @brief Header for the create_options method and a helper printer method.
+ */
 #ifndef CREATE_OPTIONS_H_
 #define CREATE_OPTIONS_H_
 
@@ -6,9 +11,8 @@
 #include <boost/program_options.hpp>
 namespace po = boost::program_options;
 
-// Prints a vector to an ostream, I think. 
-// From the boost library examples in boost_program_options
-//  file multiple_sources.cpp
+/// @brief Prints a vector to an ostream, I think. 
+/// From the boost library examples in boost_program_options file multiple_sources.cpp
 template<class T>
 std::ostream& operator<<(std::ostream& os, const std::vector<T>& v)
 {
@@ -16,7 +20,14 @@ std::ostream& operator<<(std::ostream& os, const std::vector<T>& v)
     return os;
 }
 
+/**
+ * @brief Loads options from command line and config file.
+ * @param[in] ac	main's argc: the number of command line tokens(?)
+ * @param[in] av	main's argv: the string of the command line 
+ * @param[out] visible	Loaded up with the list of Visible options (so it can be printed in main if help is called)
+ * @param[out] vm	A key-value pair map of options.
+ * @return 0 if successful, some other integer if a problem.
+ */
 int CreateOptions(int ac, char * av[], po::options_description &visible, po::variables_map &vm);
-
 
 #endif
