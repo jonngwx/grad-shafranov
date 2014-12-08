7dd5d45d454877ca5def626151b4ef9251f817be
Jonathan Ng <jonngwx@hopper12.(none)>
2014-12-07 18:41:40 -0800

    updated visualisation to use regular expressions

--- grad_output_txt.cc
+++ grad_output_txt.cc
@@ -20,28 +20,36 @@ void Grad_Output_Txt::write_output(const char* filename){
 	const int nr = grid->nr_;
     const int nz = grid->nz_;
 	// write the output to a text file
+    fprintf(file, "R: ");
 	for (int i = 0; i < nr; i++){
 		fprintf(file,"%15.8f ", grid->R_[i]);
 	}
     fprintf(file,"\n");
+    fprintf(file,"z: ");
     for (int i = 0; i < nz; i++){
 		fprintf(file,"%15.8f ", grid->z_[i]);
 	}
     fprintf(file,"\n");
+    fprintf(file,"psi: ");
     for (int i = 0; i < nr; i++){
         for (int j = 0; j < nz; j++){
             fprintf(file,"%15.8f ", f->f_[i][j]);
         }
-        fprintf(file,"\n");
     }
-    
+    fprintf(file,"\n");
+    fprintf(file,"g: ");
     for (int i = 0; i < nr; i++){
         for (int j = 0; j < nz; j++){
             fprintf(file,"%15.8f ", g->eval(f->f_[i][j]));
         }
-        fprintf(file,"\n");
     }
-    
+    fprintf(file,"\n");
+    fprintf(file,"p: ");
+    for (int i = 0; i < nr; i++){
+        for (int j = 0; j < nz; j++){
+            fprintf(file,"%15.8f ", p->eval(f->f_[i][j]));
+        }
+    }    
 	// do all other fancy outputs
     // enums?
 	fclose(file);
--- python/read_data.py
+++ python/read_data.py
@@ -1,21 +1,20 @@
 import numpy as np
 import io
-import field
 import h5py
+import re
 
-FLOAT='[0-9]*\.[0-9]*'
+FLOAT='-?[0-9]*\.[0-9]*'
 
-DATA= '([A-Za-z]): ((' + FLOAT+ ' )*)'
+DATA= '([A-Za-z]+): *((' + FLOAT+ ' *)*)'
 
 def read_hdf5(filename):
     f = h5py.File(filename,'r')
-    x = f['R'].shape[0]
-    y = f['z'].shape[0]
-    fields = field.Field(x,y)
-    fields.R = np.array(f['R'])
-    fields.z = np.array(f['z'])
-    fields.psi = np.transpose(np.array(f['psi']))
-    fields.p = np.transpose(np.array(f['p']))
+    fields = {}
+    for data in f.keys():
+        if data == 'R' or data == 'z':
+            fields[data]=np.array(f[data])
+        else:
+            fields[data]=np.transpose(np.array(f[data])
     f.close()
     return fields
 
@@ -23,28 +22,40 @@ def read_hdf5(filename):
 def read_text(filename):
     """ format assumed to be R\n,z\n, array\n, array\n"""
     f = io.open(filename,'r')
-    s = f.readline()
-    R = np.fromstring(s, sep=' ')
-    s = f.readline()
-    z = np.fromstring(s, sep=' ')
-    x = R.shape[0]
-    y = z.shape[0]
-    fields = field.Field(x,y)
-    fields.R = R
-    fields.z = z
- #   print x
- #   print y
-    for i in xrange(x):
-        s = f.readline()
-        fields.psi[i,::] = np.fromstring(s, sep = ' ')
-    fields.psi = np.transpose(fields.psi)
-    for i in xrange(x):
-        s = f.readline()
-        fields.p[i,::] = np.fromstring(s, sep = ' ')
-    fields.p = np.transpose(fields.p)
+    prog = re.compile(DATA)
+    fields = {}
+    for line in f:
+        result = prog.match(line)
+        if result is None:
+            continue
+        fields[result.group(1)] = np.fromstring(result.group(2), sep=' ')
     f.close()
+    if is_invalid(fields):
+        return None
+    nx = fields['R'].shape[0]
+    ny = fields['z'].shape[0]
+    for data in fields.keys():
+        if data == 'R' or data == 'z':
+            continue
+        else:
+            fields[data] = np.transpose(np.reshape(fields[data],[nx,ny]))
 #    print R
 #    print z
 #    print psi
 #    print p
     return fields
+
+def is_invalid(fields):
+    k = fields.keys()
+    invalid = False
+    data = ['R','z','psi','p','g']
+    for i in data:
+        invalid = (invalid or check_data(i,data))
+    return invalid
+
+def check_data(name,keys):
+    if name not in keys:
+        print name + " not in fields"
+        return True
+    else:
+        return False
