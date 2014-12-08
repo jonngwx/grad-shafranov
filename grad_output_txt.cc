bfb39f5210e62240508a05aa12d3eb596a2db4a8
Jonathan Ng <jonngwx@hopper12.(none)>
2014-12-07 19:00:11 -0800

    some cleanup of output

--- grad_output_txt.cc
+++ grad_output_txt.cc
@@ -2,6 +2,7 @@
 #include "field.h"
 #include "grid.h"
 #include <stdio.h>
+#include "util.h"
 
 Grad_Output_Txt::Grad_Output_Txt(Field* f0, Grid* grid0, RHSfunc * p0, RHSfunc * g0, const char* outputs) {
     f = f0;
@@ -18,25 +19,21 @@ void Grad_Output_Txt::write_output(const char* filename){
 	FILE * file;
 	file = fopen(filename, "w");
 	const int nr = grid->nr_;
-    const int nz = grid->nz_;
+        const int nz = grid->nz_;
 	// write the output to a text file
+        // format is name: data \n
     fprintf(file, "R: ");
-	for (int i = 0; i < nr; i++){
-		fprintf(file,"%15.8f ", grid->R_[i]);
-	}
+    print1d(file,grid->R_,nr);
     fprintf(file,"\n");
+
     fprintf(file,"z: ");
-    for (int i = 0; i < nz; i++){
-		fprintf(file,"%15.8f ", grid->z_[i]);
-	}
+    print1d(file,grid->z_,nz);
     fprintf(file,"\n");
+
     fprintf(file,"psi: ");
-    for (int i = 0; i < nr; i++){
-        for (int j = 0; j < nz; j++){
-            fprintf(file,"%15.8f ", f->f_[i][j]);
-        }
-    }
+    print2d(file,f->f_,nr,nz);
     fprintf(file,"\n");
+
     fprintf(file,"g: ");
     for (int i = 0; i < nr; i++){
         for (int j = 0; j < nz; j++){
--- include/util.h
+++ include/util.h
@@ -1,4 +1,7 @@
 /*! \file */ 
+
+#include<stdio.h>
+
 /**
 Helper function to make an array from min to max with n points
 replicates matlab's linspace
@@ -13,4 +16,19 @@ void inline linspace(double min, double max, int n, double *array){
   for (int i = 1; i < n; i++){
     array[i] = array[i-1] + (dn);
   }
+}
+
+
+inline void print1d(FILE *file, double *data, int n){
+    for (int i = 0; i < n; ++i){
+        fprintf(file, "%15.8f ", data[i]);
+    }
+}
+
+inline void print2d(FILE *file, double **data, int nx, int ny){
+    for (int i = 0; i < nx; ++i){
+        for (int j = 0; j < ny; ++j){
+            fprintf(file, "%15.8f ", data[i][j]);
+        }
+    }
 }
