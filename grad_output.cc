010c76a540d8653601fbc1c3af3d8ab7b7a57243
Jonathan Ng <jonngwx@hopper06.(none)>
2014-12-10 11:18:24 -0800

    minor edit

--- grad_output.cc
+++ grad_output.cc
@@ -4,15 +4,17 @@
 
 void Grad_Output::parse_outputs(const char *out){
     int len = strlen(out);
+    if (len < 1){
+        return;
+    }
     char outputs[len];
     strcpy(outputs,out);
-    printf("%s",outputs);
     char *tok = NULL;
     tok = strtok(outputs, ",");
     while (tok){
-      if (strcmp(tok,"test")==0){
-            printf("output is %s\n", tok);
-        }
-        tok = strtok(NULL,",");
+      if (strcmp(tok,"j")==0){
+          printf("output is %s\n", tok);
+      }
+      tok = strtok(NULL,",");
     }
 }
