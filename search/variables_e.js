62c78b004686f7ae5d789d2954c44e0948383666
Jacob Schwartz <jacobas@princeton.edu>
2015-01-04 15:48:48 -0800

    Updating docs

---
+++ search/variables_e.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['r0',['R0',['../class_critical.html#aaf115f94bf00f07010baaa057d9f11bb',1,'Critical']]],
+  ['r_5f',['R_',['../class_grid.html#a5f0c628fee6a43d8d50afa4ee9fb4f19',1,'Grid::R_()'],['../class_j_solver_alpha.html#acefe2a299149768754ef4c9465f85dc2',1,'JSolverAlpha::R_()'],['../class_slow_boundary.html#aaf155ee853a02b84a7ab44138d98cb65',1,'SlowBoundary::R_()']]],
+  ['rl',['Rl',['../class_critical.html#a2f5df63eb385bf2caca261008c44eebf',1,'Critical']]]
+];
---
+++ search/variables_f.js
@@ -0,0 +1,8 @@
+var searchData=
+[
+  ['z0',['z0',['../class_critical.html#a555465d6fc69e94b2f6b46b3ce682206',1,'Critical']]],
+  ['z_5f',['z_',['../class_grid.html#acfe207b9f2596238018440ba4e29c25e',1,'Grid::z_()'],['../class_slow_boundary.html#a80c94374d02b0f5b199c5b3c562ed0fe',1,'SlowBoundary::z_()']]],
+  ['z_5flimiter1',['z_limiter1',['../class_critical.html#ac2774f84bbaf3228b77d39611c23c0b6',1,'Critical']]],
+  ['z_5flimiter2',['z_limiter2',['../class_critical.html#a9a2dbaf1fdb38fb6f5e845475c8f4dc2',1,'Critical']]],
+  ['zl',['zl',['../class_critical.html#a10358e591b310ca1ef320f5025dbe79d',1,'Critical']]]
+];
