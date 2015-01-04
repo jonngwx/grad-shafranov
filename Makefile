915f1b3c58ed422c10f6d63a18dd28314d77495d
Jonathan Ng <jonngwx@gmail.com>
2015-01-03 18:26:21 -0800

    testing of output

--- Makefile
+++ Makefile
@@ -3,7 +3,7 @@ CXXFLAGS = -c -g -std=c++11 -Iinclude -Wall
 LIBS = -lboost_program_options -lboost_unit_test_framework -lboost_math_tr1
 HDF = -lhdf5_hl -lhdf5 -DHDF_MODE
 PROGS = gs_solver
-TESTPROGS = elliptic_test tsv_reader_example coil_data_example
+TESTPROGS = elliptic_test tsv_reader_example coil_data_example output_test
 OBJECTS = tsv_reader.o j_solver_alpha.o grid.o field.o boundary.o slow_boundary.o grad_output.o grad_output_txt.o create_options.o elliptic/sor.o elliptic/elliptic_solver.o elliptic/gauss_seidel.o elliptic/critical.o green_fcn.o
 
 .PHONY: all
@@ -19,6 +19,9 @@ gs_solver_hdf.o: gs_solver.cc
 	$(CXX) $(CXXFLAGS) $(HDF) $^ -o $@
 
 elliptic_test: elliptic/elliptic_solver.o elliptic/elliptic_test.o elliptic/sor.o grid.o field.o elliptic/gauss_seidel.o boundary.o
+	$(CXX) -o $@ $^ $(LIBS)
+
+output_test: test/test_output.o grad_output.o grad_output_txt.o field.o grid.o
 	$(CXX) -o $@ $^ $(LIBS)
 
 tsv_reader_example: tsv_reader_example.o tsv_reader.o 
--- test/test_output.cc
+++ test/test_output.cc
@@ -1,24 +1,50 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE Output
+#define protected public
+#include <boost/test/unit_test.hpp>
+#include <boost/test/floating_point_comparison.hpp>
+#include <math.h>
 #include "field.h"
 #include "grid.h"
 #include "grad_output_txt.h"
 #include <stdlib.h>
-#include "rhs_func.h"
 #include <stdio.h>
 
-int main(){
-	Field *f = new Field( 10, 11);
-    PGData *pdata = new PGData();
-    double bla[2] = {1.,2.};
-    pdata->psis = bla;
-    pdata->values = bla;
+/*!
+ * Tests the parsing of parse_output
+ */
+BOOST_AUTO_TEST_CASE (test_parse_output) {
+    double R0 = 0;
+    double Rend = 10;
+    double Z0 = -5;
+    double Zend = 5;
+    double nr = 10;
+    double nz = 10;
+    Grid *grid = new Grid(R0, Rend, Z0, Zend, nr, nz);
+    Field *psi = new Field(*grid);
+    Field *jphi = new Field(*grid);
+    Field * p =  new Field(*grid);
+    Field * g = new Field(*grid);
 
-    RHSfunc *p = new RHSfunc("blah", pdata);
-    Grid *g = new Grid(0, 0, 0 ,0 ,10, 11);
-	Grad_Output_Txt out(f, g,p,p, "foo,bar");
-	
-	out.write_output("testing");
-	delete f;
-    delete pdata;
+    Grad_Output_Txt grad_output(psi,jphi,grid,p,g,"j");
+    BOOST_CHECK_EQUAL(grad_output.output_list.size(),1);
+
+    Grad_Output_Txt grad_output1(psi,jphi,grid,p,g,"gibberish");
+    BOOST_CHECK_EQUAL(grad_output1.output_list.size(),0);
+
+    Grad_Output_Txt grad_output2(psi,jphi,grid,p,g,"j,j,j,j,j");
+    BOOST_CHECK_EQUAL(grad_output2.output_list.size(),1);
+
+    Grad_Output_Txt grad_output3(psi,jphi,grid,p,g,"j,bt");
+    BOOST_CHECK_EQUAL(grad_output3.output_list.size(),2);
+    BOOST_CHECK_EQUAL(grad_output3.output_list[1],Grad_Output::TOROIDAL_FIELD);
+    BOOST_CHECK_EQUAL(grad_output3.output_list[0],Grad_Output::CURRENT);
     delete p;
     delete g;
+    delete psi;
+    delete jphi;
+    delete grid;
+
 }
+
+
