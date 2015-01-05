077e9ab05ab3ebebe4ccaf8ff136abb0d22a8c9f
Jacob Schwartz <jacobas@princeton.edu>
2015-01-04 16:20:05 -0800

    Formatted and documented my two _example programs.

--- coil_data_example.cc
+++ coil_data_example.cc
@@ -1,24 +1,35 @@
-// a simple test of reading in some parameters to a text file.
-
+/*!
+ * @file coil_data_example.cc
+ * @author Jacob Schwartz
+ * @brief A simple program showing how to use CoilData.
+ *
+ * Demonstrates how to create a CoilData, load data from a file,
+ * and then extract the useful parameters.
+ *
+ * Not sure if we should move this and tsv_reader_example.cc to a folder called
+ * examples/ or if we should get rid of them entirely.
+ */
 #include <string>
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include "include/tsv_reader.h"
 
-int main (int argc, char * argv[]) {
+int main(int argc, char* argv[]) {
 
+  // Load data into a CoilData from file.
   std::string file_name(argv[1]);
   CoilData cd;
-  int status = cd.load_from_tsv(file_name,1);
-  if ( status != 0){
+  int status = cd.load_from_tsv(file_name, 1);
+  if (status != 0) {
     std::cout << "load from tsv not successful. exiting.\n";
     exit(1);
   }
 
+  // Print out the data.
   for (int i = 0; i < cd.num_rows(); ++i) {
-    for(int j=0; j < cd.num_columns(); ++j){
-      std::cout << cd.data(i,j) << " ";
+    for (int j = 0; j < cd.num_columns(); ++j) {
+      std::cout << cd.data(i, j) << " ";
     }
     std::cout << "\n";
   }
--- tsv_reader_example.cc
+++ tsv_reader_example.cc
@@ -1,24 +1,36 @@
-// a simple test of reading in some parameters to a text file.
-
+/*!
+ * @file tsv_reader_example.cc
+ * @author Jacob Schwartz
+ * @brief This is a short file that shows how to use Table:
+ *
+ * like how to create a Table and then load it up with data from a file
+ * and then access the relevant variables from Table: the number of columns, the
+ *number of rows, and the data itself.
+ *
+ * I'm not sure if we should put this and coil_data_example in an examples/
+ *folder or get rid of them entirely.
+ */
 #include <string>
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
 #include "include/tsv_reader.h"
 
-int main (int argc, char * argv[]) {
+int main(int argc, char* argv[]) {
 
+  // Create a table and load it with data
   std::string file_name(argv[1]);
   Table td;
-  int success = td.load_from_tsv(file_name,1);
-  if ( success != 0){
+  int success = td.load_from_tsv(file_name, 1);
+  if (success != 0) {
     std::cout << "load from tsv not successful. exiting.\n";
     exit(1);
   }
 
+  // Print out the data
   for (int i = 0; i < td.num_rows(); ++i) {
-    for(int j=0; j < td.num_columns(); ++j){
-      std::cout << td.data(i,j) << " ";
+    for (int j = 0; j < td.num_columns(); ++j) {
+      std::cout << td.data(i, j) << " ";
     }
     std::cout << "\n";
   }
