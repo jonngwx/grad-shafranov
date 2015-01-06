4e4e5bd76cb6c2129bbe014b8f0bf15c41b2c1da
Jacob Schwartz <jacobas@princeton.edu>
2015-01-06 13:31:03 -0500

    Changed a sign at Jonathan's request in alpha_g.
    Also rewrote some of the jphi calculation line (added parentheses) to
    make it clearer.

--- j_solver_alpha.cc
+++ j_solver_alpha.cc
@@ -47,7 +47,7 @@ void JSolverAlpha::update(Field *jphi, Field *psi, Field *p, Field *g) {
           //  printf("psi = %f .... p = %f \n", psi->f_[i][j], p->f_[i][j]);
         }
     }
-    double alpha_g = mu0*(-P0_*temp1 - Ip_*delta_psi/(dr_*dz_))/(0.5*g0_*g0_*temp2);
+    double alpha_g = mu0*(-P0_*temp1 + Ip_*delta_psi/(dr_*dz_))/(0.5*g0_*g0_*temp2);
     printf("alpha_g = %f \n", alpha_g);
     printf("summed jphi before update = %f\n", jtot1);
 
@@ -61,7 +61,7 @@ void JSolverAlpha::update(Field *jphi, Field *psi, Field *p, Field *g) {
                 // printf("g = %f \n", g->f_[i][j]);
                 // jphi = - p'*R - gg'/mu0/R
                 // if gg' = .5 g0^2 alphag g_s'
-                jphi->f_[i][j] = -n1_*P0_*pow(psi_s,n1_-1)*R_[i] - .5*g0_*g0_/mu0/R_[i] * alpha_g*pow(psi_s,n2_-1)*n2_;
+                jphi->f_[i][j] = -n1_*P0_*pow(psi_s,n1_-1)*R_[i] - .5*g0_*g0_ / (mu0 * R_[i]) * alpha_g*pow(psi_s,n2_-1)*n2_;
 
                 //P0_*temp1/delta_psi + g0_*g0_*alpha_g*temp2/(2*mu0*delta_psi); //update jphi
             } else {
