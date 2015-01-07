faadde0a17e33aefbcd8d0e65cdfa59ddc0d02df
Jacob Schwartz <jacobas@princeton.edu>
2015-01-06 23:23:39 -0500

    writing boundary_test.cc ... early commit so that the style of test fixtures can be seen

---
+++ test/boundary_test.cc
@@ -0,0 +1,45 @@
+/*! 
+ * @file boundary_test.cc
+ * @author Jacob Schwartz
+ * @brief Some tests for the Boundary class using the boost_test library.
+ */
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE tsv_reader_test
+#include <boost/test/unit_test.hpp>
+
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include "../include/boundary.h"
+
+struct boundarytest {
+  boundarytest() { 
+    BOOST_TEST_MESSAGE( "Setup fixture boundarytest");
+    double R0 = 0;
+    double Rend = 10;
+    double Z0 = -5;
+    double Zend = 5;
+    double nr = 3;
+    double nz = 3;
+
+    gr = new Grid(R0, Rend, Z0, Zend, nr, nz);
+    b = new Boundary(gr); 
+  }
+  ~boundarytest(){ 
+    BOOST_TEST_MESSAGE( "Teardown fixture boundarytest"); 
+    delete gr;
+    delete b;
+  }
+
+  Grid * gr;
+  Boundary * b;
+
+};
+
+BOOST_FIXTURE_TEST_SUITE( suite1, boundarytest)
+
+BOOST_AUTO_TEST_CASE(LToI) {
+  BOOST_CHECK_EQUAL(b->LtoI(0),0);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
