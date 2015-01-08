223496076ab542a5e10d595454e66694f4720f63
Jacob Schwartz <jacobas@princeton.edu>
2015-01-08 16:52:06 -0500

    updating documentation format and wording for interpolate.h

--- include/interpolate.h
+++ include/interpolate.h
@@ -32,11 +32,11 @@ public:
     void updateCoefficients();
     void updateP(double r, double z);
 private:
-    double r_curr; /** < i coordinate of center of current interpolation */
-    double z_curr; /** < j coordinate of center of current interpolation */
-    Field &Psi_; /** < */
-    Grid &Grid_; /** < Underlying grid */
-    std::vector<std::vector<double>> P; /** < Array of 4 points corresponding to surrounding used for interpolation */
+    double r_curr; //!< i coordinate of center of current interpolation.
+    double z_curr; //!< j coordinate of center of current interpolation.
+    Field &Psi_; //!<
+    Grid &Grid_; //!< Underlying grid.
+    std::vector<std::vector<double>> P; //!< 4x4 points that are the values at the grid nodes around the point being interpolated.
     double a00, a01, a02, a03, a10, a11, a12, a13, a20, a21, a22, a23, a30, a31, a32, a33;
 };
 
