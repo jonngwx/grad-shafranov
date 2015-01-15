0c066fbd8fc583287314b0f266ef4ffc7d621102
Jacob Schwartz <jacobas@princeton.edu>
2015-01-14 22:26:44 -0500

    what I have to say about testing
    Also updates to the test results though grid size = 80

--- runTimeTester.py
+++ runTimeTester.py
@@ -3,6 +3,6 @@
 from subprocess import call
 import os
 
-for n in range(32, 71):
+for n in range(71, 81):
     print "n is %d" % (n)
     os.system("./gs_solver --grid-elems-r %d --grid-elems-z %d | tail -n 1 | awk '{ print \"%d,\", $4, $8}' >> runTimeResults.txt" % (n,n,n))
