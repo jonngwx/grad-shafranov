8acfd9e6985e35f278690561a00ce202ad74d584
Peter Bolgert <pbolgert@pppl.gov>
2015-01-15 15:12:28 -0500

    slow boundary test will pass now -- made grid finer

--- test/slow_boundary_test.cc
+++ test/slow_boundary_test.cc
@@ -9,86 +9,7 @@
 #include "gauss_seidel.h"
 #include "../include/green_fcn.h"
 
-/*!
- * This roundabout test involves the consistency
- * of green_fcn.cc, slow_boundary.cc, and gauss_seidel.cc.
- *  
- */
 
-BOOST_AUTO_TEST_CASE (Shaf_Sol_Very_Slow) {
-
-    /***********************************
-     * Specify parameters for the grid
-     **********************************/
-    double Rmin = 4.0;
-    double Rmax = 8.0;
-    double zmin = -2.0;
-    double zmax = 2.0;
-    int nr = 20;
-    int nz = 20;
-  
-    double max_iter = 1000000;
-    double epsilon = 1e-8;
-  //  double perim = 2.0*(nr + nz -2.0);
-    Grid *grid = new Grid(Rmin, Rmax, zmin, zmax, nr, nz);
-    Field *psi = new Field(*grid);
-    Field *psi_old = new Field(*grid);
-    Field *jphi = new Field(*grid);
-    Boundary *psib = new SlowBoundary(psi, grid);
-    /*********************************
-     * Specify parameters for SS model
-     *********************************/
-    double B0 = 1.0; // B field in Teslas at mag axis
-    double R0 = 6.0; // radial position of mag axis
-    double kap0 = 1.1; // ellipticity of plasma
-    double q0 = 2.01; // safety factor at mag axis
-    double eps = 0.2; // inverse aspect ratio
-    const double mu0 = 4*M_PI*1e-7; // mag perm of free space
-    double psi_pv = eps*eps*kap0*R0*R0*B0/(2*q0); // defines psi at plas/vac interf.
-    double R,Z;
-    double coeff = B0/(R0*R0*kap0*q0);
-    for (int i = 0; i < grid->nr_; ++i) {
-      R = grid->R_[i];
-      for (int j = 0; j < grid->nz_; ++j) {
-	Z = grid->z_[j];
-	psi_old->f_[i][j] = (coeff/2.0)*(R*R*Z*Z + (kap0*kap0/4.0)*pow((R*R-R0*R0),2));
-	psi->f_[i][j] = psi_old->f_[i][j]+1;
-	if ((psi_old->f_[i][j] >= 0) && (psi_old->f_[i][j] <= psi_pv)) {
-	  jphi->f_[i][j] = R*coeff*(kap0*kap0 + 1)/mu0;
-	}
-	else {
-	  jphi->f_[i][j] = 0;
-	} 
-      }
-    }
-
-    /*********************************
-     * Calculate new boundary for psi
-     *********************************/
-    psib->CalcB(psi); // updates the boundary grid points of psi
-
-    
-    // Solve for psi given the proper boundary conditionsi
-    double error_ES = 0.01;
-    EllipticSolver *solver = new GaussSeidel(*grid, *psi, error_ES);
-    solver->coeff();
-    for (int n = 0; n < max_iter; ++n) {
-        if (n==0) solver->step_1(*jphi);
-        else solver->step(*jphi);
-//	printf("n = %d\n, norm = %f\n",n,solver->norm());
-        if (solver->norm() < epsilon) {
-	  break;
-
-	}
-    }
-
-    // Compare selected points between psi and 
-    // psi_green (psi calculated using green
-    // functions competely.
-    
-
-
-}
 
 BOOST_AUTO_TEST_CASE(Green_Fcn_Test){
 
@@ -99,8 +20,8 @@ BOOST_AUTO_TEST_CASE(Green_Fcn_Test){
     double Rmax = 7.0;
     double zmin = -1.0;
     double zmax = 1.0;
-    int nr = 250;
-    int nz = 250;
+    int nr = 500;
+    int nz = 500;
     int perim = 2*(nr + nz - 2);  
 
     Grid *grid = new Grid(Rmin, Rmax, zmin, zmax, nr, nz);
@@ -120,7 +41,7 @@ BOOST_AUTO_TEST_CASE(Green_Fcn_Test){
     double eps = 0.3; // inverse aspect ratio
     const double mu0 = 4*M_PI*1e-7; // mag perm of free space
     double psi_pv = eps*eps*kap0*R0*R0*B0/(2*q0); // defines psi at plas/vac interf.
-    printf("psi_pv = %f \n", psi_pv);
+    // printf("psi_pv = %f \n", psi_pv);
     double R,z;
     double coeff = B0/(R0*R0*kap0*q0);
     for (int i = 0; i < grid->nr_; ++i) {
